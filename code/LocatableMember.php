a3347e4d4a8b88156cd04f285aa514471531612b
Josh Kosmala <josh@novaweb.co.nz>
2015-08-17 15:15:00 +1200

    Reinstating address fields

--- code/LocatableMember.php
+++ code/LocatableMember.php
@@ -4,8 +4,13 @@ class LocatableMember extends DataExtension {
 
 	// Extending SilverStripe MemberProfiles to include lat/lng
 	static $db = array(
-			"Longitude" => "Varchar",
-			"Latitude" => "Varchar"
+		"Address1" => "Text",
+		"Address2" => "Text",
+		"Suburb" => "Varchar",
+		"City" => "Varchar",
+		"Region" => "Enum('Northland,Auckland,Coromandel,Waikato,Bay Of Plenty,East Cape,Taupo,King Country,Taranaki,Hawkes Bay,Wanganui,Wellington,Wairarapa,Nelson,Marlborough,West Coast,Canterbury,Otago,Southland', 'Auckland')",
+		"Longitude" => "Varchar",
+		"Latitude" => "Varchar"
 	);
 
 }
