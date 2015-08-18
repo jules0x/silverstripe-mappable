b41c6ea1b2fe7b2f82e3b523baef65e2b8fcd08f
Josh Kosmala <josh@novaweb.co.nz>
2015-08-19 11:32:36 +1200

    Comments and tidy up. Error warning.

--- code/LocatableMember.php
+++ code/LocatableMember.php
@@ -9,25 +9,33 @@ class LocatableMember extends DataExtension {
 		"Suburb" => "Varchar",
 		"City" => "Varchar",
 		"Region" => "Enum('Northland,Auckland,Coromandel,Waikato,Bay Of Plenty,East Cape,Taupo,King Country,Taranaki,Hawkes Bay,Wanganui,Wellington,Wairarapa,Nelson,Marlborough,West Coast,Canterbury,Otago,Southland', 'Auckland')",
-		"Longitude" => "Varchar",
-		"Latitude" => "Varchar"
+		"Latitude" => "Varchar",
+		"Longitude" => "Varchar"
 	);
 
 	public function onBeforeWrite() {
-		self::convertAddressToPoint();
+		// Convert address to Latitude/Longitude
+		$this->convertAddressToPoint();
 		parent::onBeforeWrite();
 	}
 
-	function convertAddressToPoint() {
-		$records = Member::get();
-		foreach($records as $record) {
-			if (!$record->Latitude) {
-				$address = $record->Address1 . " " . $record->Suburb . " " . $record->Region;
-				$point = GoogleGeocoding::address_to_point($address);
-				$record->Latitude = $point['Latitude'];
-				$record->Longitude = $point['Longitude'];
-				//$record->write();
+	private function convertAddressToPoint() {
+		// Get the current member
+		$member = Member::get()->byID($this->owner->ID);
+		// if Latitude is not populated, there must be no Geocoding saved in the database, let's populate it
+		if(!$member->Latitude) {
+			if(!$member->Address1){
+				user_error('The member needs to have an address', E_USER_ERROR);
+        		exit();
 			}
+			// Get the member's address
+			$address = $member->Address1 . " " . $member->Suburb . " " . $member->Region;
+			// Run it through GoogleGeocoding's address_to_point function (RestfulService/XML)
+			$point = GoogleGeocoding::address_to_point($address);
+			// Set the Latitude and Longitude values with the reponse
+			$member->Latitude = $point['Latitude'];
+			$member->Longitude = $point['Longitude'];
+			return;
 		}
 	}
 
