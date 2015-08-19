5a275faa7bf69d47c201c5b070dad8ef64bfc671
Josh Kosmala <josh@novaweb.co.nz>
2015-08-19 12:36:33 +1200

    Beautify and comments

--- code/LocationMapPage.php
+++ code/LocationMapPage.php
@@ -1,43 +1,47 @@
 <?php
 
-class LocationMapPage extends Page {
-
+class LocationMapPage extends Page
+{
 }
 
-class LocationMapPage_Controller extends Page_Controller {
+class LocationMapPage_Controller extends Page_Controller
+{
+    public function init()
+    {
+        parent::init();
+        Requirements::javascript('framework/thirdparty/jquery/jquery.js');
+        Requirements::javascript('http://maps.googleapis.com/maps/api/js?key=AIzaSyAAaa_ApoYASmy5j35SKI7q1UcLzvdxf2E&sensor=false');
+        Requirements::javascript(MODULE_MAPPABLE_DIR.'/javascript/GoogleMapConfig.js');
+        Requirements::javascriptTemplate(MODULE_MAPPABLE_DIR.'/javascript/GoogleMapConfig.js', array(
+                    'memberCount' => $this->MemberCountForMap(),
+                    'content' => $this->MapMemberData(),
+                    ));
+        Requirements::css(MODULE_MAPPABLE_DIR.'/css/mappable.css');
+                //self::convertAddressToPoint();
+    }
 
-		public function init() {
-			parent::init();
-		Requirements::javascript("framework/thirdparty/jquery/jquery.js");
-		Requirements::javascript("http://maps.googleapis.com/maps/api/js?key=AIzaSyAAaa_ApoYASmy5j35SKI7q1UcLzvdxf2E&sensor=false");
-		Requirements::javascript(MODULE_MAPPABLE_DIR.'/javascript/GoogleMapConfig.js');
-		Requirements::javascriptTemplate(MODULE_MAPPABLE_DIR.'/javascript/GoogleMapConfig.js', array(
-					'memberCount' => $this->MemberCountForMap(),
-					'content' => $this->MapMemberData()
-					));
-		Requirements::css(MODULE_MAPPABLE_DIR."/css/mappable.css");
- 				//self::convertAddressToPoint();
-		}
+    public function MemberCountForMap()
+    {
+        $members = Member::get();
+        $memberCount = $members->Count();
 
-		function MemberCountForMap() {
-			$members = Member::get();
-			$memberCount = $members->Count();
-			return $memberCount;
-		}
+        return $memberCount;
+    }
 
-		function MapMemberData() {
-			$members = Member::get();
-			$contentArray = array();
-			foreach($members as $member){
-				array_push($contentArray, "<strong>" . $member->FirstName . " " . $member->Surname . "</strong>");
-			}
-			$contentOut = implode("*", $contentArray);
-			return $contentOut;
-		}
+    public function MapMemberData()
+    {
+        $members = Member::get();
+        $contentArray = array();
+        foreach ($members as $member) {
+            array_push($contentArray, '<strong>'.$member->FirstName.' '.$member->Surname.'</strong>');
+        }
+        $contentOut = implode('*', $contentArray);
 
-		function Map() {
-			return '<div id="map_canvas"></div>';
-		}
+        return $contentOut;
+    }
 
-
+    public function Map()
+    {
+        return '<div id="map_canvas"></div>';
+    }
 }
